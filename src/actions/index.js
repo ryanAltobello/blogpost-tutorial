d535d29936998929cd5723ac17077041781f965a
ryanAltobello <rchap504@gmail.com>
2019-01-08 15:48:47 -0600

    final commit

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,30 @@
 import jsonPlaceholder from "../apis/jsonPlaceholder";
+import _ from "lodash";
+
+export const fetchPostsAndUsers = () => async (dispatch, getState) => {
+  await dispatch(fetchPosts());
+
+  // ---------- GETTING UNIQUE IDS OF ALL USERS ----------
+
+  // const userIds = _.uniq(_.map(getState().posts, "userId"));
+  // userIds.forEach(id => dispatch(fetchUser(id)));
+
+  // Alternate way to approach
+  _.chain(getState().posts)
+    .map("userId")
+    .uniq()
+    .forEach(id => dispatch(fetchUser(id)))
+    .value();
+};
 
 export const fetchPosts = () => async dispatch => {
   const response = await jsonPlaceholder.get("/posts");
 
-  dispatch({ type: "FETCH_POSTS", payload: response });
+  dispatch({ type: "FETCH_POSTS", payload: response.data });
+};
+
+export const fetchUser = id => async dispatch => {
+  const response = await jsonPlaceholder.get(`/users/${id}`);
+
+  dispatch({ type: "FETCH_USER", payload: response.data });
 };
